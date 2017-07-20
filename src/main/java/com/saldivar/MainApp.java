9337a94ae01bb1964e71540cd7bbf0c6b70b15d8
Jorge Saldivar <1724724+jorgesaldivar@users.noreply.github.com>
2017-07-19 23:06:55 -0500

    Docs install and GUI

--- src/main/java/com/saldivar/MainApp.java
+++ src/main/java/com/saldivar/MainApp.java
@@ -7,6 +7,16 @@ import org.springframework.data.mongodb.core.MongoOperations;
 import com.saldivar.config.SpringConfig;
 import com.saldivar.model.User;
 
+/**
+ * Install MongoDB using Ubuntu
+ * https://docs.mongodb.com/manual/tutorial/install-mongodb-enterprise-on-ubuntu/
+ * 
+ * Download compass (Mongodb GUI):
+ * https://www.mongodb.com/download-center?filter=enterprise#compass
+ * 
+ * @author saldivar
+ *
+ */
 public class MainApp {
 
 	public static void main(String[] args) {
